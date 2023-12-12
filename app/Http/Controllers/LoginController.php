766e940f787f8b80e1410c2bb2bc55cbb1996cb5
Hikuroshi <92168188+Hikuroshi@users.noreply.github.com>
2023-12-12 11:29:44 +0700

    update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -14,29 +14,54 @@ class LoginController extends Controller
             'title' => 'Login',
         ]);
     }
-
+    
     public function authenticate(Request $request)
     {
         $credentials = $request->validate([
             'username' => 'required',
             'password' => 'required'
         ]);
-
+        
         if (Auth::attempt($credentials)){
             $request->session()->regenerate();
             User::where('id', auth()->user()->id)->update(['last_login' => now()]);
             
             return redirect()->intended('/');
         }
-
+        
         return back()->with('loginError', 'Gagal Login!');
     }
-
+    
     public function logout()
     {
         Auth::logout();
         request()->session()->invalidate();
         request()->session()->regenerateToken();
         return redirect('/');
+    }
+    
+    public function apiLogin(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+        
+        if (Auth::attempt($credentials)) {
+            $user = $request->user();
+            $token = $user->createToken('token-name')->plainTextToken;
+            
+            return response()->json(['token' => $token], 200);
+        }
+        
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+    
+    public function apiLogout(Request $request)
+    {
+        $user = $request->user();
+        $user->tokens()->delete();
+        
+        return response()->json(['message' => 'Logged out successfully'], 200);
     }
 }
