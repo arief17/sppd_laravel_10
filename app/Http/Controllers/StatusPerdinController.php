5077f9039061b596787b68909f05e82c307ea501
Hikuroshi <92168188+Hikuroshi@users.noreply.github.com>
2023-12-18 10:59:21 +0700

    test Query Builder

--- app/Http/Controllers/StatusPerdinController.php
+++ app/Http/Controllers/StatusPerdinController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\StatusPerdin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StatusPerdinController extends Controller
 {
@@ -28,7 +29,13 @@ class StatusPerdinController extends Controller
     public function apiApprove(Request $request)
     {
         $id = $request->get('id');
-        StatusPerdin::where('id', $id)->update(['approve' => 1]);
+        DB::table('data_perdins')
+            ->join('status_perdins','data_perdins.status_id','=','status_perdins.id')
+            ->where('data_perdins.id',$id)
+            ->update([
+                'status_perdins.approve' => 1,
+            ]);
+        // StatusPerdin::where('id', $id)->update(['approve' => 1]);
         return response()->json(['message' => 'Status Perdin berhasil diapprove'], 200);
         // return response()->json(['message' => $id], 200);
     }
