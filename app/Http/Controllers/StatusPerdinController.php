a5591d9465fa41f80ad66a131b996320e3f2742f
Hikuroshi <92168188+Hikuroshi@users.noreply.github.com>
2023-12-18 07:18:47 +0700

    update

--- app/Http/Controllers/StatusPerdinController.php
+++ app/Http/Controllers/StatusPerdinController.php
@@ -18,13 +18,13 @@ class StatusPerdinController extends Controller
         $validatedData = request()->validate([
             'alasan_tolak' => 'required',
         ]);
-
+        
         $validatedData['approve'] = 0;
         
         StatusPerdin::where('id', $id)->update($validatedData);
         return redirect()->route('data-perdin.index', 'no_laporan')->with('success', 'Status Perdin berhasil diperbarui!');
     }
-
+    
     public function apiApprove(Request $request)
     {
         $id = $request->query('id');
@@ -34,14 +34,16 @@ class StatusPerdinController extends Controller
     
     public function apiTolak(Request $request)
     {
-        $validatedData = request()->validate([
-            'alasan_tolak' => 'required',
-        ]);
-
-        $validatedData['approve'] = 0;
         $id = $request->query('id');
+        $alasan_tolak = $request->query('alasan_tolak');
         
-        StatusPerdin::where('id', $id)->update($validatedData);
+        $dataToUpdate = [
+            'approve' => 0,
+            'alasan_tolak' => $alasan_tolak,
+        ];
+        
+        StatusPerdin::where('id', $id)->update($dataToUpdate);
+        
         return response()->json(['message' => 'Status Perdin berhasil ditolak'], 200);
     }
 }
