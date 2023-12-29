eb132c47cd1121881fe6dda762a17fd0921bd088
Hikuroshi <92168188+Hikuroshi@users.noreply.github.com>
2023-12-29 17:41:36 +0700

    update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -14,24 +14,24 @@ class LoginController extends Controller
             'title' => 'Login',
         ]);
     }
-    
+
     public function authenticate(Request $request)
     {
         $credentials = $request->validate([
             'username' => 'required',
             'password' => 'required'
         ]);
-        
+
         if (Auth::attempt($credentials)){
             $request->session()->regenerate();
             User::where('id', auth()->user()->id)->update(['last_login' => now()]);
-            
+
             return redirect()->intended('/');
         }
-        
+
         return back()->with('loginError', 'Gagal Login!');
     }
-    
+
     public function logout()
     {
         Auth::logout();
@@ -39,30 +39,30 @@ class LoginController extends Controller
         request()->session()->regenerateToken();
         return redirect('/');
     }
-    
+
     public function apiLogin(Request $request)
     {
         $credentials = $request->validate([
             'username' => 'required',
             'password' => 'required'
         ]);
-        
+
         if (Auth::attempt($credentials)) {
             $user = $request->user();
-            $token = $user->createToken('token-name')->plainTextToken;
-                        
-            return response()->json(['token' => $token, 'user' => $user], 200);
+            $token = $user->createToken($request->token_name);
+
+            return response()->json(['token' => $token->plainTextToken, 'user' => $user], 200);
         }
-        
+
         return response()->json(['message' => 'Invalid credentials'], 401);
     }
-    
-    
+
+
     public function apiLogout(Request $request)
     {
         $user = $request->user();
         $user->tokens()->delete();
-        
+
         return response()->json(['message' => 'Logged out successfully'], 200);
     }
 }
