d65d3bb38a559b6501cde94115cf21e2058debab
Hikuroshi <92168188+Hikuroshi@users.noreply.github.com>
2024-01-31 10:50:40 +0700

    update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -203,7 +203,7 @@
 								</ul>
 							</li>
 							@endcan
-							<li class="slide {{ request()->routeIs('data-perdin*', 'laporan-perdin*', 'kwitansi-perdin*', 'rekap-pegawai', 'rekap-bidang') ? 'is-expanded' : '' }}">
+							<li class="slide {{ request()->routeIs('data-perdin*', 'rekap-pegawai', 'rekap-bidang') ? 'is-expanded' : '' }}">
 								<a class="side-menu__item" data-bs-toggle="slide" href="javascript:void(0);">
 									<svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M13 4H6v16h12V9h-5V4zm3 14H8v-2h8v2zm0-6v2H8v-2h8z" opacity=".3"></path><path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"></path></svg>
 									<span class="side-menu__label">Perdin</span>
@@ -248,8 +248,6 @@
 											</li>
 										</ul>
 									</li>
-									<li><a class="slide-item {{ request()->routeIs('laporan-perdin.index') ? 'active' : '' }}" href="{{ route('laporan-perdin.index') }}">Arsip Laporan </a></li>
-									<li><a class="slide-item {{ request()->routeIs('kwitansi-perdin.index') ? 'active' : '' }}" href="{{ route('kwitansi-perdin.index') }}">Laporan Bendahara </a></li>
 									@can('isAdmin')
 									<li class="sub-slide {{ request()->routeIs('rekap-pegawai', 'rekap-bidang') ? 'is-expanded' : '' }}">
 										<a class="slide-item" data-bs-toggle="sub-slide" href="javascript:void(0);">
@@ -262,6 +260,24 @@
 									</li>
 									@endcan
 								</ul>
+							</li>
+							<li class="slide {{ request()->routeIs('laporan-perdin*') ? 'is-expanded' : '' }}">
+								<a class="side-menu__item {{ request()->routeIs('laporan-perdin.index') ? 'active' : '' }}" href="{{ route('laporan-perdin.index') }}">
+									<svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24" >
+										<path d="M0 0h24v24H0V0z" fill="none"/><path d="M5 5h4v6H5zm10 8h4v6h-4zM5 17h4v2H5zM15 5h4v2h-4z" opacity=".3"/>
+										<path d="M3 13h8V3H3v10zm2-8h4v6H5V5zm8 16h8V11h-8v10zm2-8h4v6h-4v-6zM13 3v6h8V3h-8zm6 4h-4V5h4v2zM3 21h8v-6H3v6zm2-4h4v2H5v-2z"/>
+									</svg>
+									<span class="side-menu__label">Arsip Laporan</span>
+								</a>
+							</li>
+							<li class="slide {{ request()->routeIs('kwitansi-perdin*') ? 'is-expanded' : '' }}">
+								<a class="side-menu__item {{ request()->routeIs('kwitansi-perdin.index') ? 'active' : '' }}" href="{{ route('kwitansi-perdin.index') }}">
+									<svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24" >
+										<path d="M0 0h24v24H0V0z" fill="none"/><path d="M5 5h4v6H5zm10 8h4v6h-4zM5 17h4v2H5zM15 5h4v2h-4z" opacity=".3"/>
+										<path d="M3 13h8V3H3v10zm2-8h4v6H5V5zm8 16h8V11h-8v10zm2-8h4v6h-4v-6zM13 3v6h8V3h-8zm6 4h-4V5h4v2zM3 21h8v-6H3v6zm2-4h4v2H5v-2z"/>
+									</svg>
+									<span class="side-menu__label">Laporan Bendahara</span>
+								</a>
 							</li>
 						</ul>
 						<div class="slide-right" id="slide-right"><svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewBox="0 0 24 24"><path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"/></svg></div>
